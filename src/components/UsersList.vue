f259c7ebe4da9f99679e33e8c33f6424454b2e1c
Kudrina Anastasia <anastasia.kd@hectasoft.ru>
2025-05-18 23:30:01 +0300

    Home work N2: Users list

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,37 +1,97 @@
 <template>
-    <ul>
-        <user-list-item
-            v-for="user in users"
-            :key="user.id"
-        />
-    </ul>
+    <div class="users">
+        <div class="title">
+            <h2>Список пользователей</h2>
+
+            <div class="link" @click="showUsers = !showUsers">
+                {{ showUsers ? 'Скрыть' : 'Показать' }}
+            </div>
+        </div>
+
+        <div v-show="showUsers" class="users-list">
+            <UsersListItem v-for="user in users" :key="user.id" :user="user" />
+        </div>
+    </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import UsersListItem from './UsersListItem.vue';
 
 const users = [
     {
         id: 1,
         name: 'Воланд',
-        description: 'повелитель сил Тьмы',
-        age: null
-    }, 
+        description: 'Повелитель сил Тьмы',
+        age: null,
+    },
     {
         id: 2,
         name: 'Азазелло',
-        description: 'посланник дьявола',
-        age: null
-    }, 
+        description: 'Посланник дьявола',
+        age: null,
+    },
     {
         id: 3,
         name: 'Коровьев-Фагот',
-        description: 'маг, регент, чародей, переводчик или черт его знает кто на самом деле',
-        age: null
-    }
-]
+        description: 'Маг, регент, чародей, переводчик или черт его знает кто на самом деле',
+        age: null,
+    },
+    {
+        id: 4,
+        name: 'Кот Бегемот',
+        description: 'Лучший из шутов',
+        age: null,
+    },
+    {
+        id: 5,
+        name: 'Гелла',
+        description: '«...служанку мою Геллу рекомендую. Расторопна, понятлива, и нет такой услуги, которую она не сумела бы оказать»',
+        age: null,
+    },
+    {
+        id: 6,
+        name: 'Мастер',
+        description: '— Вы — писатель? — с интересом спросил поэт.'
+            + '\nГость потемнел лицом и погрозил Ивану кулаком, потом сказал:'
+            + '\n— Я — мастер…',
+        age: 38,
+    },
+    {
+        id: 7,
+        name: 'Маргарита',
+        description: 'Ежегодно мессир дает один бал. Он называется весенним балом полнолуния, или балом ста королей… Так вот-с: мессир холост, как вы, конечно, сами понимаете. Но нужна хозяйка',
+        age: 30,
+    },
+];
+
+const showUsers = ref(true);
 </script>
 
 <style lang="scss" scoped>
+.users {
+    margin: 2rem auto;
+    max-width: 40rem;
+    min-width: 21rem;
+    width: 80%;
 
+    .title {
+        align-items: center;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 2rem;
+
+        h2 {
+            flex: 1
+        }
+    }
+
+    &-list {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        width: 100%
+    }
+}
 </style>
--- src/components/UsersListItem.vue
+++ src/components/UsersListItem.vue
@@ -1,11 +1,20 @@
 <template>
-    <li class="user-item">
-        <h1>{{ user.name }}</h1>
-        <span v-if="user.age" class="user-item-age">
-            {{ user.age }}
-        </span>
-        <p>{{ user.description }}</p>
-    </li>
+    <div class="user-item">
+        <div class="title">
+            <h3>{{ user.name }}</h3>
+
+            <div class="age">
+                <template v-if="user.age">
+                    ({{ user.age }})
+                </template>
+                <template v-else>
+                    (Бессмертный)
+                </template>
+            </div>
+        </div>
+
+        <div class="descripton">{{ user.description }}</div>
+    </div>
 </template>
 
 <script setup>
@@ -13,8 +22,38 @@ defineProps({
     user: Object,
 })
 
+const bgColor = '#FFFFFF';
+const hoveredBgColor = '#F8FEFB';
 </script>
 
 <style lang="scss" scoped>
-.user-item {}
+.user-item {
+    background: v-bind(bgColor);
+    border: 1px solid #0AAF604D;
+    border-radius: 0.5rem;
+    box-sizing: border-box;
+    padding: 1.5rem;
+    transition: all 0.3s ease-out;
+    width: 100%;
+
+    &:hover {
+        background-color: v-bind(hoveredBgColor);
+    }
+
+    .title {
+        display: flex;
+        gap: 0.5rem;
+    }
+
+    .age {
+        color: #AAAAAA;
+    }
+
+    .descripton {
+        color: #AAAAAA;
+        margin-top: 0.5rem;
+        font-size: 0.875rem;
+        white-space: pre-wrap;
+    }
+}
 </style>
