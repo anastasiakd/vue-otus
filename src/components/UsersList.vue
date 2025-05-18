cce4954c462ff39ecb6e6f54c6fb7740c144b0ec
Kudrina Anastasia <anastasia.kd@hectasoft.ru>
2025-05-18 23:23:36 +0300

    Home work N2: Users list

---
+++ src/components/UsersList.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul>
+        <user-list-item
+            v-for="user in users"
+            :key="user.id"
+        />
+    </ul>
+</template>
+
+<script setup>
+import UsersListItem from './UsersListItem.vue';
+
+const users = [
+    {
+        id: 1,
+        name: 'Воланд',
+        description: 'повелитель сил Тьмы',
+        age: null
+    }, 
+    {
+        id: 2,
+        name: 'Азазелло',
+        description: 'посланник дьявола',
+        age: null
+    }, 
+    {
+        id: 3,
+        name: 'Коровьев-Фагот',
+        description: 'маг, регент, чародей, переводчик или черт его знает кто на самом деле',
+        age: null
+    }
+]
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/UsersListItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li class="user-item">
+        <h1>{{ user.name }}</h1>
+        <span v-if="user.age" class="user-item-age">
+            {{ user.age }}
+        </span>
+        <p>{{ user.description }}</p>
+    </li>
+</template>
+
+<script setup>
+defineProps({
+    user: Object,
+})
+
+</script>
+
+<style lang="scss" scoped>
+.user-item {}
+</style>
